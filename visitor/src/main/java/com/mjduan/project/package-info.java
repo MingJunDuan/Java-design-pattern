9c4992f5dd38a036f074c47f8f6aee8af85959ce
MingJunDuan <mjduan@yahoo.com>
2017-09-06 10:57:59 +0800

    [design-pattern-2.0] visitor-pattern

---
+++ visitor/src/main/java/com/mjduan/project/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Reference: https://github.com/rafadelnero/design-patterns-saga/tree/master/design-patterns-saga/src/main/java/com/designpatternsaga/visitor
+ *
+ *
+ * Hans 2017-09-06 10:56
+ */
+package com.mjduan.project;
